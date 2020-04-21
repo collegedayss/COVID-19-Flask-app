4e72d7fe7b0c002c3d871b3d359f627060b13d12
Curzuv <alexandr.chebatarev@gmail.com>
2020-04-21 19:07:36 -0400

    flask added

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
+import os
 app = Flask(__name__)
+data = {}
+data["content"] = ""
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if data:
+        return render_template('index.html', data=data)
+    return render_template('index.html')
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    
+    if request.method == 'POST':
+        f = request.files['file']
+        print()
+        f.save(os.path.join('images/', f.filename))
+        data["content"] = f.filename
+    return redirect(url_for('index'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
